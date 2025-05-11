7b4b5e6335f2edfd18985de42cd22d4e2a96664d
Utkarsh Raj Srivastava <utkarshrajsrivastava7@gmail.com>
2025-05-11 12:24:57 +0530

    chat bot init

--- internal/groq/chat.go
+++ internal/groq/chat.go
@@ -1,12 +1,26 @@
 package groq
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
+	"net/http"
 	"os"
 )
 
-func GetPositiveResponse(userInput string) (string, error) {
+func GetPositiveResponse(userInput string) ([]string, error) {
 	apiKey := os.Getenv("GROQ_API_KEY")
-	fmt.Println(".env", apiKey)
-	return apiKey, nil
+	url := " https://api.groq.com/openai/v1/chat/completions"
+
+	reqBody := map[string]interface{}{
+		"model": "llama-3.3-70b-versatile",
+		"message": []map[string]string{
+			{"role": "system", "content": "You are a kind uplifting chatbot, always respond with something positive and the person talking to you is going through ups and downs in his life"},
+			{"role": "user", "content": userInput},
+		},
+	}
+
+	bodyByte, _ := json.Marshal(reqBody)
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(bodyByte))
+	req.Header.Set("Content-Type", "application/json")
+	return []string{apiKey, url}, nil
 }
