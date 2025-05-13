d0cd1d9e3d384c24374d8a407b324be636274974
hsraktu17 <utkarsh172002srivastava@gmail.com>
2025-05-13 18:45:01 +0530

    server working added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
@@ -9,4 +13,9 @@ func main() {
 			"message": "ok",
 		})
 	})
+
+	log.Println("Server is running on port 8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
 }
