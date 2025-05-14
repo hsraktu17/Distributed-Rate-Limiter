1a1544afe784b739f0df724a154651c84d2c0d20
hsraktu17 <utkarsh172002srivastava@gmail.com>
2025-05-14 14:45:12 +0530

    limiter.go logic

--- internal/limiter/limiter.go
+++ internal/limiter/limiter.go
@@ -21,3 +21,23 @@ func NewLimiter(rate, burst int) *Limiter {
 		lastCheck: time.Now(),
 	}
 }
+
+func (l *Limiter) Allow() bool {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	now := time.Now()
+	elapsed := now.Sub(l.lastCheck).Seconds()
+	l.lastCheck = now
+
+	l.token += int(elapsed * float64(l.rate))
+	if l.token > l.burst {
+		l.token = l.burst
+	}
+
+	if l.token > 0 {
+		l.token--
+		return true
+	}
+	return false
+}
